9ffc148fc2ee52b3785a3b1d52727b16c396e8a1
Guillaume CAMUS <guillaume.camus@manomano.com>
2018-09-21 23:45:23 +0200

    chore: refactor dot-file

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Installation
-    
+# Guiyomh's Dot Files
+
+These are the various config files I have to setup a system the way I want it.
+
+## Installation
+
 ```bash
 ➜ git clone https://github.com/guiyomh/dot-files.git ~/.dotfiles
 ➜ cd ~/.dotfiles
--- install.sh
+++ install.sh
@@ -1,39 +1,61 @@
 #!/usr/bin/env bash
+#
+# This installation is destructive, as it removes exisitng files/directories.
+# Use at your own risk.
 
-command_exists() {
-    type "$1" > /dev/null 2>&1
+# Get path to the current script
+SCRIPT_NAME="$(basename ${BASH_SOURCE[0]})"
+pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
+SCRIPT_DIR=$(pwd)
+popd > /dev/null
+
+# names in this list won't be dot-prefixed
+skip_dot_prefix=(nvim)
+
+# contains checks if an array ($2) contains a given element ($1).
+contains() {
+    local e match="$1"
+    shift
+    for e; do [[ "$e" == "$match" ]] && return 0; done
+    return 1
 }
 
-echo "Installing dotfiles."
+# copy or symlink all the dotfiles
+UNAME=$(uname | tr '[:upper:]' '[:lower:]')
+for path in $SCRIPT_DIR/*; do
+    name=$(basename $path)
+    case $name in
+        *.md|*.sh) continue;;
+    esac
 
-echo "Initializing submodule(s)"
-git submodule update --init --recursive
+    # If there exists a platform-specific version, then use that
+    if [ -e "${path}.${UNAME}" ]; then
+        echo "Using platform-specific ${name} for ${UNAME}"
+        path="${path}.${UNAME}"
+    fi
 
-source install/link.sh
-source install/git.sh
+    # If the file is in the skip dot list, then we don't add a dot
+    target="$name"
+    if ! contains "$name" "${skip_dot_prefix[@]}"; then
+        target=".$name"
+    fi
 
-# only perform macOS-specific install
-if [ "$(uname)" == "Darwin" ]; then
-    echo -e "\\n\\nRunning on macOS"
-    
-    # source install/xcode.sh
-    source install/brew.sh
-    source install/osx.sh
-    source install/osx.fix.sh
-fi
+    # Build our complete path to the home directory
+    target="$HOME/tmp-home/$target"
+    if [ -h $target -o -f $target ]; then
+        rm $target
+    elif [ -d $target ]; then
+        rm -rf $target
+    fi
 
-echo "creating vim directories"
-mkdir -p ~/.vim-tmp
-
-source install/nvim.sh
-source install/zsh.sh
-
-if ! command_exists zsh; then
-    echo "zsh not found. Please install and then re-run installation scripts"
-    exit 1
-elif ! [[ $SHELL =~ .*zsh.* ]]; then
-    echo "Configuring zsh as default shell"
-    chsh -s "$(command -v zsh)"
-fi
-
-echo "Done. Reload your terminal."
+    case $UNAME in
+        cygwin* | mingw32*)
+        cp -R $path "$target"
+        echo "Copied $name to $target."
+        ;;
+        *)
+        ln -s $path "$target"
+        echo "Linked $name to $target."
+        ;;
+    esac
+done
